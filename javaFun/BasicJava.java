d38fed48db3027ff23c86d3b6f5c7348ae48fd65
Radek Wazny <radek.wazny@gmail.com>
2021-04-09 03:15:28 -0500

    BasicJava

--- javaFun/BasicJava.java
+++ javaFun/BasicJava.java
@@ -1,201 +1,166 @@
-import javax.management.ObjectName;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class BasicJava {
-    public void print0To255() {
 
-        for (int i=0; i<=255; i++) {
-            System.out.println(i);
-        }
-    }
-    public void printOdd() {
-        for (int i=0; i<=255; i+=2) {
-            System.out.println(i);
-        }
-    }
-    public void printSum(){
+/* printing functions */
 
-        int total = 0;
-        for (int i=1; i<=255; i++) {
-            total+=i;
-            System.out.println("New number:  " + i + " Sum: " + total);
-        }
-    }
-    public void printArray(ArrayList arr){
+    public void printArray(ArrayList arr) {
 
-        for (Object el: arr ) {
-
+        for (Object el: arr) {
             System.out.println(el);
         }
     }
-
-    public int findMax(int [] arr) {
-
-        int maxValue = Integer.MIN_VALUE;
-
-        for ( int n: arr) {
-            if (n > maxValue) {
-                maxValue = n;
-            }
+    public void print1to255() {
+        for (int n = 1; n<=255; n++) {
+            System.out.println(n);
         }
-
-        return maxValue;
-
     }
 
+    public void printOdd1to255() {
+        for (int n = 1; n <= 255; n += 2) {
+            System.out.println(n);
+        }
+    }
+
+        public void printSum() {
+        int sum = 0;
+
+        for (int i =0; i<256; i++) {
+
+            sum+=i;
+            System.out.println("New number" + i + " Sum " + sum);
+        }
+    }
+
+    /** Find max **/
     public int findMax(ArrayList<Integer> arr) {
 
-        int maxValue = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
 
-        for ( int n: arr) {
-            if (n > maxValue) {
-                maxValue = n;
-            }
+        for (int el: arr) {
+            if (el > max) max = el;
         }
 
-        return maxValue;
-
+        return max;
     }
-
     public int findMin(ArrayList<Integer> arr) {
+        int min = Integer.MAX_VALUE;
 
-        int minValue = Integer.MAX_VALUE;
+        for (int el: arr) {
+            if (el < min) min = el;
+        }
 
-        for ( int n: arr) {
-            if (n < minValue) {
-                minValue= n;
+        return min;
+    }
+
+    public int[] getMaxMinAvg(ArrayList<Integer> numbers) {
+
+        return new int[] {findMax(numbers), findMin(numbers), getAverage(numbers)};
+    }
+    public int getAverage(ArrayList<Integer> numbers) {
+
+        // if there are no numbers return -1
+        if (numbers.size() == 0) return -1;
+
+        return sum(numbers)/numbers.size();
+
+    }
+
+    public ArrayList<Integer> eliminateNegatives(ArrayList<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+
+            int x = numbers.get(i);
+
+            if (x < 0) {
+                x = 0;
+
             }
+            numbers.set(i, x);
+
+
         }
-
-        return minValue;
-
+        return numbers;
     }
+    public ArrayList<Integer> square(ArrayList<Integer> numbers) {
 
-
-
-
-    public double getAvg(int [] arr) {
-
-        double sum = getSum(arr);
-
-
-        if (arr.length > 0) {
-            return sum / arr.length;
-        }
-        else {
-            return -1.0;
-        }
-    }
-
-    public double getAvg(ArrayList<Integer> arr) {
-
-        double sum = getSum(arr);
-
-
-        if (arr.size()> 0) {
-            return sum / arr.size();
-        }
-        else {
-            return -1.0;
-        }
-    }
-
-
-    private ArrayList<Integer> range(int from, int to, int step) {
-        ArrayList<Integer> res = new ArrayList<>();
-        for (int i = from; i<=to;i+=step ){
-            res.add(i);
+        for (int i =0; i<numbers.size(); i++ ){
+            int x = numbers.get(i);
+            numbers.set(i,x*x);
         }
 
-        return res;
-    }
-    private ArrayList<Integer> range(int from, int to) {
-        int step = 1;
-        ArrayList<Integer> res = new ArrayList<>();
-        for (int i = from; i<=to;i+=step ){
-           res.add(i);
-        }
-
-        return res;
-    }
-
-    private double getSum(ArrayList<Integer> arr) {
-        int total = 0;
-        for (int a: arr) {
-            total+=a;
-        }
-
-        return total;
-    }
-    private double getSum(int [] arr) {
-        int total = 0;
-        for (int a: arr) {
-            total+=a;
-        }
-
-        return total;
+        return numbers;
     }
     public ArrayList<Integer> arrayWithOddNumbers() {
-
-        return range(1, 255,2);
+        return range(1,255,2);
     }
-    public int greaterThanY(int [] arr, int y){
-
+    public int countGreaterThanY(ArrayList<Integer> numbers, int y) {
         int count = 0;
-
-        for (int n:arr) {
-
-           if (n > y ) { count++;}
+        for (int n : numbers) {
+            if (n>y) {
+                count++;
+            }
         }
-
         return count;
     }
+/** Helper function to sum up an array / list used by getAverage **/
+    private int sum(ArrayList<Integer> nums) {
 
-    public ArrayList<Integer> square(int [] arr) {
 
-        ArrayList<Integer> res = new ArrayList<Integer>();
+        int total = 0;
+        for (int n : nums) {
+            total += n;
 
-        for (int a: arr) {
-            res.add(a*a);
+        }
+
+        return total;
+    }
+
+    /** Helper Methods to generate ranges **/
+    private ArrayList<Integer> range(int from, int to) {
+        return range(from, to, 1);
+    }
+    private ArrayList<Integer> range(int from, int to, int step) {
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = from; i<=to; i+=step) {
+            result.add(i);
 
         }
 
-        return res;
+        return result;
     }
 
-    public ArrayList<Integer> removeNegatives(int[] arr) {
 
-        ArrayList<Integer> res = new ArrayList<Integer>();
-        for (int a: arr) {
-            if (a>=0) {
-                res.add(a);
-            }
+    public ArrayList<Integer> shiftArray(ArrayList<Integer> numbers) {
 
-        }
-        return res;
-    }
-
-    public int[] maxMinAvg(ArrayList<Integer> e) {
+        numbers.remove(0);
+        numbers.add(0);
 
 
-        return new int[]{
-                findMax(e), findMax(e), (int) getAvg(e)};
-
-
-
-    }
-
-    public ArrayList shiftValues(ArrayList e) {
-
-        return e;
+        return numbers;
     }
 
     public static void main(String[] args) {
 
-        BasicJava basic = new BasicJava();
+        BasicJava basicJava = new BasicJava();
+        ArrayList<Integer> al = new ArrayList<>(Arrays.asList(2,3,4,5));
+        ArrayList<Integer> negatives = new ArrayList<>(Arrays.asList(-2, -5, 0, 1));
+        ArrayList<Integer> shifted= new ArrayList<>(Arrays.asList(1,5,10,7,-2));
 
-        System.out.println(basic.findMax(new int[]{1, 2, 3}));
-        System.out.println(basic.findMax(new int[]{1, -2, 3}));
+
+        System.out.println(basicJava.square(al));
+        System.out.println(basicJava.shiftArray(shifted));
+        System.out.println(basicJava.eliminateNegatives(negatives));
+        System.out.println(basicJava.arrayWithOddNumbers());
+        System.out.println(Arrays.toString(basicJava.getMaxMinAvg(al)));
+        System.out.println(basicJava.countGreaterThanY(al,10));
+
+        basicJava.print1to255();
+        basicJava.printOdd1to255();
+        basicJava.printArray(al);
+        basicJava.printSum();
+
     }
-
 }
